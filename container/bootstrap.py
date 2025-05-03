1139df067101d473a1d73e6da8d7c3a710cfc8a2
Jack Turk <jackturk5@outlook.com>
2025-05-03 01:52:36 -0500

    more socat

--- container/bootstrap.py
+++ container/bootstrap.py
@@ -60,7 +60,7 @@ Popen([
 # 2) Relay outgoing packets from BoringTun → FD 3
 Popen([
     'socat',
-    'UDP-LISTEN:51820,bind=127.0.0.1,reuseaddr,fork',
+    'UDP-LISTEN:51820,reuseaddr,fork',
     'UDP-SENDTO:127.0.0.1:51820'
 ], close_fds=False)
 
