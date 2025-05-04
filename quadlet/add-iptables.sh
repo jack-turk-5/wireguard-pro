c560d5e3f22e43ddf3d59599770702ffa58913e1
Jack Turk <jackturk5@outlook.com>
2025-05-04 14:14:04 -0500

    short paths??

--- quadlet/add-iptables.sh
+++ quadlet/add-iptables.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 /usr/bin/podman exec wireguard-pro \
-/usr/sbin/iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE && \
-/usr/sbin/iptables -A FORWARD -i wg0 -o eth0 -j ACCEPT && \
-/usr/sbin/iptables -A FORWARD -i eth0 -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE && \
+iptables -A FORWARD -i wg0 -o eth0 -j ACCEPT && \
+iptables -A FORWARD -i eth0 -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
--- quadlet/remove-iptables.sh
+++ quadlet/remove-iptables.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 /usr/bin/podman exec wireguard-pro \
-/usr/sbin/iptables -t nat -D POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE &&
-/usr/sbin/iptables -D FORWARD -i wg0 -o eth0 -j ACCEPT &&
-/usr/sbin/iptables -D FORWARD -i eth0 -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -D POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE &&
+iptables -D FORWARD -i wg0 -o eth0 -j ACCEPT &&
+iptables -D FORWARD -i eth0 -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
