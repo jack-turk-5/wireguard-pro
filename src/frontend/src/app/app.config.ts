4ba0d6cf2ad51dab21792af4138c0475747a8d87
Jack Turk <jackturk5@outlook.com>
2025-05-15 23:59:58 -0500

    app config for storage

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -1,8 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, PLATFORM_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { JwtInterceptor } from './auth/jwt.interceptor';
+import { LOCAL_STORAGE } from './storage/storage';
+import { isPlatformServer } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-};
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), provideClientHydration(withEventReplay()), 
+    provideHttpClient(
+      withInterceptors([JwtInterceptor]), 
+      withFetch()
+    ), 
+    provideAnimationsAsync(),
+    {
+      provide: LOCAL_STORAGE,
+      useFactory: (platformId: object) => {
+   	 if (isPlatformServer(platformId)) {
+   	   return {}; // Return an empty object on the server
+   	 }
+   	 return localStorage; // Use the browser's localStorage
+      },
+      deps: [PLATFORM_ID],
+    }
+  ]
+}
--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,18 +1,6 @@
-// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter }       from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { JwtInterceptor } from './app/auth/jwt.interceptor';
-import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(
-      withInterceptors([JwtInterceptor])
-    )
-  ]
-})
-.catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
