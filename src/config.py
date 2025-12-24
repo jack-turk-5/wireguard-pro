1ded7f0be8977e3658a6888baec58ba67f93cb6d
jack <jackturk5@outlook.com>
2025-12-24 15:54:27 -0600

    Fix circular dep

--- src/config.py
+++ src/config.py
@@ -3,11 +3,12 @@ import asyncio
 from os import environ
 from itsdangerous import URLSafeTimedSerializer as Serializer
 
-from utils import get_server_pubkey
+from utils import get_server_pubkey, _run_command
 
 
 class AppConfig:
     """A singleton class to hold application configuration."""
+
     def __init__(self):
         self.secret_key = None
         self.wg_endpoint = None
@@ -30,25 +31,36 @@ class AppConfig:
             if self._is_loaded:
                 return
 
-            self.secret_key = environ.get('SECRET_KEY')
-            self.wg_endpoint = environ.get('WG_ENDPOINT')
+            self.secret_key = environ.get("SECRET_KEY")
+            self.wg_endpoint = environ.get("WG_ENDPOINT")
 
             if not self.secret_key or not self.wg_endpoint:
-                raise Exception('Missing SECRET_KEY and/or WG_ENDPOINT')
+                raise Exception("Missing SECRET_KEY and/or WG_ENDPOINT")
 
-            self.ts = Serializer(self.secret_key, salt='auth-token')
+            self.ts = Serializer(self.secret_key, salt="auth-token")
             self.wg_public_key = await get_server_pubkey()
-            self.wg_allowed_ips = environ.get('WG_ALLOWED_IPS', '0.0.0.0/0, ::/0')
-            self.wg_dns_server = environ.get('WG_DNS_SERVER', '1.1.1.1')
+            self.wg_allowed_ips = environ.get("WG_ALLOWED_IPS", "0.0.0.0/0, ::/0")
+            self.wg_dns_server = environ.get("WG_DNS_SERVER", "1.1.1.1")
             self.wg_ipv4_base_addr = environ.get("WG_IPV4_BASE_ADDR", "10.8.0.1")
-            self.wg_ipv6_base_addr = environ.get("WG_IPV6_BASE_ADDR", "fd86:ea04:1111::1")
-            
+            self.wg_ipv6_base_addr = environ.get(
+                "WG_IPV6_BASE_ADDR", "fd86:ea04:1111::1"
+            )
+
             self._is_loaded = True
             logging.info("Successfully loaded server public key and config.")
+
+    async def get_server_pubkey(self):
+        """
+        Derive server public key from stored private key asynchronously.
+        """
+        async with open("/etc/wireguard/privatekey") as f:
+            priv = (await f.read()).strip()
+        return await _run_command("wg pubkey", stdin_input=priv)
 
 
 # Create a single, shared instance of the config
 config = AppConfig()
+
 
 async def get_config() -> AppConfig:
     """
